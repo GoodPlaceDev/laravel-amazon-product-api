32b7e4703486f61ef9f6bcabe7d6c90ade68f80d
Jun Morimoto <j-morimoto@good-place.co.jp>
2020-07-29 11:26:45 +0900

    added "parameter" argument to AmazonProduct::search

--- src/Concerns/Search.php
+++ src/Concerns/Search.php
@@ -13,7 +13,7 @@ trait Search
     /**
      * {@inheritdoc}
      */
-    public function search(string $category, string $keyword = null, int $page = 1)
+    public function search(string $category, string $keyword = null, int $page = 1, array $parameters = null)
     {
         /**
          * @var SearchItemsResource[] $resources
@@ -27,6 +27,14 @@ trait Search
         $request->setPartnerTag(Config::get('amazon-product.associate_tag'));
         $request->setPartnerType(PartnerType::ASSOCIATES);
         $request->setResources($resources);
+
+        if (!empty($parameters)) {
+            $setters = $request->setters();
+            foreach ($parameters as $key => $value) {
+                $setter = $setters[$key];
+                $request->$setter($value);
+            }
+        }
 
         $request = $this->callHook('search', $request);
 
